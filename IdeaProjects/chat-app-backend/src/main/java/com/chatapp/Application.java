c56cdf574689e24ff92daf4a77d94c0812137161
hadija <hahmatovic1@etf.unsa.ba>
2024-07-18 21:02:41 +0200

    renamed package and main Application class

--- IdeaProjects/chat-app-backend/src/main/java/com/chatapp/Application.java
+++ IdeaProjects/chat-app-backend/src/main/java/com/chatapp/Application.java
@@ -1,13 +1,13 @@
-package com.ksljivo1;
+package com.chatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatAppBackendApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatAppBackendApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
