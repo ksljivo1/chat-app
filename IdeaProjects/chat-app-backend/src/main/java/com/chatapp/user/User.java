1989e7936ba83b617b16476287da88e78a5d3353
ksljivo1 <ksljivo1@etf.unsa.ba>
2024-07-20 16:00:10 +0200

    Replace explicit constructors, getters and setters with Lombok annotations

---
+++ IdeaProjects/chat-app-backend/src/main/java/com/chatapp/user/User.java
@@ -0,0 +1,23 @@
+package com.chatapp.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
